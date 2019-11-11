3a3bec1726c7bc103a0d464adf607a62d5fc8431
kastair <kennaalstair@gmail.com>
2019-11-11 09:52:53 -0800

    post request for '/hash'

--- api/api-router.js
+++ api/api-router.js
@@ -6,8 +6,22 @@ const usersRouter = require('../users/users-router.js');
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 
+let bcrypt = require('bcryptjs');
+
 router.get('/', (req, res) => {
-  res.json({ api: "It's alive" });
+	res.json({ api: "It's alive" });
+});
+
+router.post('/hash', (req, res) => {
+	// read a password from the body
+	// hash the password using bcryptjs
+	// return it to the user in an object that looks like
+	// { password: 'original passsword', hash: 'hashed password' }
+	const original = req.body.password;
+	const credentials = req.body;
+	const hash = bcrypt.hashSync(credentials.password, 14);
+	credentials.password = hash;
+	res.json({ originalPassword: original, hashedPassword: hash });
 });
 
 module.exports = router;
